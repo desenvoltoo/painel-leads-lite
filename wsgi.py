0554fe3cd24c764b28dc3533874a3390ceb95dc1
desenvoltoo <matheuscosta.tec@icloud.com>
2026-07-27 16:47:42 -0300

    Ativa compatibilidade da página de importações na gestão

--- wsgi.py
+++ wsgi.py
@@ -82,16 +82,19 @@ try:
     from services.unifecaf_import_recovery import start_unifecaf_import_recovery
     from services.anhanguera_import_recovery import start_anhanguera_import_recovery
     from services.unifecaf_serial_guard import apply_unifecaf_serial_guard
+    from services.management_import_compat import apply_management_import_compat
 
     produtividade_export_module._is_matriculated = _matriculado_explicito
     multi_result = apply_multi_institution_metrics()
     cache_result = apply_gestao_response_cache()
     serial_result = apply_unifecaf_serial_guard()
+    management_import_result = apply_management_import_compat()
 
     application = create_app()
     application.logger.info("Gestão multi-instituição ativada: %s", multi_result)
     application.logger.info("Cache da Gestão ativado: %s", cache_result)
     application.logger.info("Serialização da SP UniFECAF ativada: %s", serial_result)
+    application.logger.info("Importações da Gestão corrigidas: %s", management_import_result)
     register_institution_routes(application)
     register_upload_preview_routes(application)
     register_upload_new_only_routes(application)
